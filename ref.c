077d645883dce99a065f5e4179b10dc5b6f9ccf6
yenquang000 <yenq98662@gmail.com~>
2026-03-04 18:28:31 -0500

    testing

---
+++ ref.c
@@ -0,0 +1,31 @@
+// No includes needed, the tracer will add stdio.h
+float calculate_average(int scores[], int min_score, int num_scores)
+{
+    int total = 0;
+    int count = 0;
+    for (int i = 0; i < num_scores; i++)
+    {
+        if (scores[i] >= min_score)
+        {
+            total += scores[i];
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        return 0.0f;
+    }
+    float avg = (float)total / count;
+    return avg;
+}
+
+// Main function to run the code
+int main()
+{
+    int test_scores[] = {100, 80, 50, 90, 70};
+    int min_val = 65;
+    int num = 5;
+    float final_avg = calculate_average(test_scores, min_val, num);
+    printf("Final Average: %f\\n", final_avg);
+    return 0;
+}
